e8ee129180d560e37d4c71a08986e8b47763ee13
Student09 <Student@III-EEIT>
2015-06-24 11:29:09 +0800

    commit test1.java 多幾行

--- pro1/src/pro1/Test1.java
+++ pro1/src/pro1/Test1.java
@@ -4,6 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
 
 	}
 
