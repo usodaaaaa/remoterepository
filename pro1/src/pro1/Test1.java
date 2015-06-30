5e4d37efa64517fb6af248ab0318e4dfd061af25
Student09 <Student@III-EEIT>
2015-06-30 10:11:09 +0800

    commit n time

--- pro1/src/pro1/Test1.java
+++ pro1/src/pro1/Test1.java
@@ -5,12 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println(123);
 		System.out.println(123);
-		System.out.println(123);
-		System.out.println(123);
-		System.out.println(123);
-		System.out.println(123);
-		System.out.println(123);
-		System.out.println(123);
+		System.out.println(100);
+		System.out.println(100);
 
 	}
 
